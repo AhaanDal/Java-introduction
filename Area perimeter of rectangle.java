ffc14f573bb5e7c70b7e23edd6f40534dcf987e9
AhaanDal <Ahaandalmia2012@gmail.com>
2025-07-03 14:11:02 +0530

    Area perimeter of rectangle.java
    Area and perimeter of a rectangle

---
+++ Area perimeter of rectangle.java
@@ -0,0 +1,17 @@
+public class Main
+  {
+    int johnage= 14;
+public static void main (String[] args)
+    {
+   
+      float length = 12;
+      float breadth = 6;
+      float area = length *breadth;
+      float perimeter = 2*(length + breadth);
+      System.out.printf("The area of the rectangle is %f\nThe permimeter of the rectangle is %f\n", area, perimeter);
+      
+        
+    
+  }
+  }
+// This is a program to calculate the area and perimeter of a rectangle
